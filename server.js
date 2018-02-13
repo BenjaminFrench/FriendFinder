d6d670d7abc414b951b9dbc1bc4e2259830f0a79
Ben French <ben.jminw@gmail.com>
2018-02-13 11:22:53 -0700

    Initial files.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+// Tells node that we are creating an "express" server
+var app = express();
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
